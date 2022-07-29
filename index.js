10d65bcb70ae16d34f43bbaf2e30376168fa8eda
Sakina <sakina.ibrahimova@pipedrive.com>
2022-07-29 16:19:10 +0300

    added

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const Koa = require("koa");
 const app = new Koa();
 
-//const router = require("./lib/routes/post-routes");
+const bodyParser = require("koa-bodyparser");
+app.use(bodyParser());
 
-//app.use(router.routes());
+const router = require("./lib/routes/posts-router");
+app.use(router.routes());
 
 app.listen(3000, () =>
   console.log("Server is running at http://localhost:3000")
